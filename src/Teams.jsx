14b84b2c8a41ae83e8aba28c3bd4c4a40fc76c81
adibmannan1 <adibmannan1@gmail.com>
2024-02-08 07:27:27 +0600

    arrow icon added

--- src/Teams.jsx
+++ src/Teams.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Teams = ({selectedTeam, handleTeamSelectionChange}) => {
   return (
     <select className='select-box' value={selectedTeam} onChange={handleTeamSelectionChange}>
-        <option value="Team A">Team A</option>
-        <option value="Team B">Team B</option>
-        <option value="Team C">Team C</option>
-        <option value="Team D">Team D</option>
+      <option value="Team A">Team A</option>
+      <option value="Team B">Team B</option>
+      <option value="Team C">Team C</option>
+      <option value="Team D">Team D</option>
     </select>
   )
 }
